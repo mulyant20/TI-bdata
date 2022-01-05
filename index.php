e2e1fa7c8c40c449dd02ccba0d8a9f7d94679e24
mulyant20 <mulyan.t20@gmail.com>
2022-01-06 00:42:03 +0700

    add index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+    require 'func.php';
+
+    $mhs = query("SELECT * FROM sewa");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equi v="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+    <table>
+        <tr>
+            <th>id</th>
+            <th>id_alat</th>
+            <th>id_customer</th>
+            <th>tanggal keluar</th>
+            <th>tanggal kembali</th>
+            <th>kuantitas</th>
+            <th>subtotal</th>
+            <th>action</th>
+        </tr>
+        <?php foreach($mhs as $row) : ?>
+        <tr>
+            <td><?= $row["id_sewa"] ?></td>
+            <td><?= $row["id_alat"] ?></td>
+            <td><?= $row["id_customer"] ?></td>
+            <td><?= $row["tggl_keluar"] ?></td>
+            <td><?= $row["tggl_kembali"] ?></td>
+            <td><?= $row["kuantitas"] ?></td>
+            <td><?= $row["subtotal"] ?></td>
+            <td>
+                <a href="edit.php?id=<?= $row["id"] ?>">edit</a>
+                <a href="hapus.php?id=<?= $row["id"] ?>">hapus</a>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+    <a href="tambah.php">tambah</a>
+</body>
+</html>
